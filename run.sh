d070d32cbe2e97528da35990c192eeb998a71d57
qkoqhh <qkoqhh@163.com>
2023-11-09 17:35:30 +0800

    shell

--- run.sh
+++ run.sh
@@ -3,18 +3,20 @@ target_dir=$2
 
 exe_file="TugraphCompetition-jar-with-dependencies.jar"
 
-class="com.antgroup.geaflow.case1and4.case1.Run"
-echo "CASE::$class"
-java -cp .:$exe_file $class  $data_dir $target_dir
+java -jar $exe_file $data_dir $target_dir
 
-class="com.antgroup.geaflow.casetwo.CaseTwo"
-echo "CASE::$class"
-java -cp .:$exe_file $class  $data_dir $target_dir
-
-class="com.antgroup.geaflow.casethree.CaseThree"
-echo "CASE::$class"
-java -cp .:$exe_file $class  $data_dir $target_dir
-
-class="com.antgroup.geaflow.case1and4.case4.Run"
-echo "CASE::$class"
-java -cp .:$exe_file $class  $data_dir $target_dir
+#class="com.antgroup.geaflow.case1and4.case1.Run"
+#echo "CASE::$class"
+#java -cp .:$exe_file $class  $data_dir $target_dir
+#
+#class="com.antgroup.geaflow.casetwo.CaseTwo"
+#echo "CASE::$class"
+#java -cp .:$exe_file $class  $data_dir $target_dir
+#
+#class="com.antgroup.geaflow.casethree.CaseThree"
+#echo "CASE::$class"
+#java -cp .:$exe_file $class  $data_dir $target_dir
+#
+#class="com.antgroup.geaflow.case1and4.case4.Run"
+#echo "CASE::$class"
+#java -cp .:$exe_file $class  $data_dir $target_dir
